8f9d917c46d22684cf987eb1fcf4f2395725fca9
Aaron <piafyoyo06@gmail.com>
2025-01-25 17:28:32 +0800

    新增 ProductList.jsx

---
+++ src/component/ProductList.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ReactLoading from "react-loading";
+import { currency } from "../utils/filter";
+
+function ProductList({
+  products,
+  openModal,
+  addCart,
+  loadingProductId,
+  loadingCartId,
+}) {
+  return (
+    <table className="table align-middle">
+      <thead>
+        <tr>
+          <th>圖片</th>
+          <th>產品名稱</th>
+          <th>價錢</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map((product) => (
+          <tr key={product.id}>
+            <td style={{ width: "200px" }}>
+              <div
+                style={{
+                  height: "100px",
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  backgroundImage: `url(${product.imageUrl})`,
+                }}
+              />
+            </td>
+            <td>{product.title}</td>
+            <td>
+              <del className="h6">
+                原價： {currency(product.origin_price)} 元
+              </del>
+              <div className="h5">特價： {currency(product.price)} 元</div>
+            </td>
+            <td>
+              <div className="btn-group btn-group-sm">
+                <button
+                  className="btn btn-outline-secondary"
+                  onClick={() => openModal(product.id)}
+                  disabled={loadingProductId === product.id}>
+                  {loadingProductId === product.id ? (
+                    <ReactLoading
+                      type="spin"
+                      color="#6c757d"
+                      height={20}
+                      width={20}
+                    />
+                  ) : (
+                    "查看更多"
+                  )}
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-outline-danger"
+                  onClick={() => addCart(product.id, 1)}
+                  disabled={loadingCartId === product.id}>
+                  {loadingCartId === product.id ? (
+                    <ReactLoading
+                      type="spin"
+                      color="#dc3545"
+                      height={20}
+                      width={20}
+                    />
+                  ) : (
+                    "加入購物車"
+                  )}
+                </button>
+              </div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+ProductList.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+      title: PropTypes.string.isRequired,
+      imageUrl: PropTypes.string.isRequired,
+      origin_price: PropTypes.number.isRequired,
+      price: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+  openModal: PropTypes.func.isRequired,
+  addCart: PropTypes.func.isRequired,
+  loadingProductId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  loadingCartId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+export default ProductList;
