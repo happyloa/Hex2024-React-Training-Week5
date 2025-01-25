721b0bd7310a7bfbcedd218ed4adc7654dd87ecc
Aaron <piafyoyo06@gmail.com>
2025-01-25 18:08:13 +0800

    拆分表單元件

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Pagination from "./component/Pagination";
 import ProductDetailModal from "./component/ProductDetailModal";
 import ProductList from "./component/ProductList";
 import Cart from "./component/Cart";
+import PlaceOrderForm from "./component/PlaceOrderForm";
 
 const API_BASE = import.meta.env.VITE_API_BASE;
 const API_PATH = import.meta.env.VITE_API_PATH;
@@ -219,101 +220,12 @@ function App() {
 
       {/* 表單資料 */}
       <div className="mt-5 row justify-content-center">
-        <form onSubmit={handleSubmit(onSubmit)} className="col-md-6">
-          <div className="mb-3">
-            <label htmlFor="name" className="form-label">
-              收件人姓名
-            </label>
-            <input
-              id="name"
-              type="text"
-              className="form-control"
-              placeholder="請輸入姓名"
-              {...register("name", { required: "請輸入收件人姓名。" })}
-            />
-            {errors.name && (
-              <p className="text-danger">{errors.name.message}</p>
-            )}
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              Email
-            </label>
-            <input
-              id="email"
-              type="email"
-              className="form-control"
-              placeholder="請輸入 Email"
-              {...register("email", {
-                required: "請輸入 Email。",
-                pattern: { value: /^\S+@\S+$/i, message: "Email 格式不正確。" },
-              })}
-            />
-            {errors.email && (
-              <p className="text-danger">{errors.email.message}</p>
-            )}
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="tel" className="form-label">
-              收件人電話
-            </label>
-            <input
-              id="tel"
-              type="tel"
-              className="form-control"
-              placeholder="請輸入電話"
-              {...register("tel", {
-                required: "請輸入收件人電話。",
-                minLength: {
-                  value: 8,
-                  message: "電話號碼至少需要 8 碼。",
-                },
-                pattern: {
-                  value: /^\d+$/,
-                  message: "電話號碼格式不正確，僅限數字。",
-                },
-              })}
-            />
-            {errors.tel && <p className="text-danger">{errors.tel.message}</p>}
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="address" className="form-label">
-              收件人地址
-            </label>
-            <input
-              id="address"
-              type="text"
-              className="form-control"
-              placeholder="請輸入地址"
-              {...register("address", { required: "請輸入收件人地址。" })}
-            />
-            {errors.address && (
-              <p className="text-danger">{errors.address.message}</p>
-            )}
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="message" className="form-label">
-              留言
-            </label>
-            <textarea
-              id="message"
-              className="form-control"
-              placeholder="留言"
-              rows="3"
-              {...register("message")}
-            />
-          </div>
-
-          <div className="text-end">
-            <button type="submit" className="btn btn-danger">
-              送出訂單
-            </button>
-          </div>
-        </form>
+        <PlaceOrderForm
+          handleSubmit={handleSubmit}
+          onSubmit={onSubmit}
+          register={register}
+          errors={errors}
+        />
       </div>
     </div>
   );
