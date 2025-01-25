0a956b3ac981c2832fba433b396a3bd826120f1c
Aaron <piafyoyo06@gmail.com>
2025-01-25 17:52:50 +0800

    移除查看購物車按鈕

--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -14,122 +14,106 @@ function Cart({ cart, deleteCart, deleteCartAll, updateCart }) {
     });
   }, []);
 
-  // 打開 Offcanvas
-  const openCart = () => {
-    offcanvasRef.current.show();
-  };
-
   return (
-    <>
-      {/* Offcanvas Trigger */}
-      <button
-        className="btn btn-outline-primary"
-        type="button"
-        onClick={openCart}>
-        查看購物車
-      </button>
-
-      {/* Offcanvas */}
-      <div
-        className="offcanvas offcanvas-end"
-        tabIndex="-1"
-        id="cartOffcanvas"
-        aria-labelledby="cartOffcanvasLabel">
-        <div className="offcanvas-header">
-          <h5 className="offcanvas-title" id="cartOffcanvasLabel">
-            購物車
-          </h5>
-          <button
-            type="button"
-            className="btn-close"
-            data-bs-dismiss="offcanvas"
-            aria-label="Close"></button>
-        </div>
-        <div className="offcanvas-body">
-          {/* 購物車內容 */}
-          {cart?.carts?.length > 0 ? (
-            <>
-              <table className="table align-middle">
-                <thead>
-                  <tr>
-                    <th></th>
-                    <th>品名</th>
-                    <th>數量/單位</th>
-                    <th>單價</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {cart.carts.map((item) => (
-                    <tr key={item.id}>
-                      <td>
-                        <button
-                          type="button"
-                          className="btn btn-outline-danger btn-sm"
-                          onClick={() => deleteCart(item.id)}>
-                          <i className="bi bi-x" /> 刪除
-                        </button>
-                      </td>
-                      <td>{item.product.title}</td>
-                      <td>
-                        <div className="input-group input-group-sm">
-                          <input
-                            type="number"
-                            className="form-control"
-                            min="1"
-                            defaultValue={item.qty}
-                            key={item.qty}
-                            onChange={(e) =>
-                              updateCart(item.id, Number(e.target.value))
-                            }
-                          />
-                          <div className="input-group-text">
-                            /{item.product.unit}
-                          </div>
-                        </div>
-                      </td>
-                      <td className="text-end">
-                        {item.final_total !== item.total && (
-                          <small className="text-success">折扣價：</small>
-                        )}
-                        {currency(item.final_total)}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-                <tfoot>
-                  <tr>
-                    <td colSpan="3" className="text-end">
-                      總計
-                    </td>
-                    <td className="text-end">{currency(cart?.total)}</td>
-                  </tr>
-                  {cart?.final_total !== cart?.total ? (
-                    <tr>
-                      <td colSpan="3" className="text-end text-success">
-                        折扣價
-                      </td>
-                      <td className="text-end text-success">
-                        {currency(cart?.final_total)}
-                      </td>
-                    </tr>
-                  ) : null}
-                </tfoot>
-              </table>
-              <div className="text-end">
-                <button
-                  className="btn btn-outline-danger"
-                  type="button"
-                  onClick={deleteCartAll}>
-                  清空購物車
-                </button>
-              </div>
-            </>
-          ) : (
-            <p>您的購物車是空的。</p>
-          )}
-        </div>
+    <div
+      className="offcanvas offcanvas-end text-nowrap"
+      tabIndex="-1"
+      id="cartOffcanvas"
+      aria-labelledby="cartOffcanvasLabel">
+      <div className="offcanvas-header">
+        <h5 className="offcanvas-title" id="cartOffcanvasLabel">
+          購物車
+        </h5>
+        <button
+          type="button"
+          className="btn-close"
+          data-bs-dismiss="offcanvas"
+          aria-label="Close"></button>
       </div>
-    </>
+      <div className="offcanvas-body">
+        {/* 購物車內容，依照有無內容來判斷顯示的元素 */}
+        {cart?.carts?.length > 0 ? (
+          <>
+            <table className="table align-middle">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>品名</th>
+                  <th>數量/單位</th>
+                  <th>單價</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cart.carts.map((item) => (
+                  <tr key={item.id}>
+                    <td>
+                      <button
+                        type="button"
+                        className="btn btn-outline-danger btn-sm"
+                        onClick={() => deleteCart(item.id)}>
+                        <i className="bi bi-x" /> 刪除
+                      </button>
+                    </td>
+                    <td>{item.product.title}</td>
+                    <td>
+                      <div className="input-group input-group-sm">
+                        <input
+                          type="number"
+                          className="form-control"
+                          min="1"
+                          defaultValue={item.qty}
+                          key={item.qty}
+                          onChange={(e) =>
+                            updateCart(item.id, Number(e.target.value))
+                          }
+                        />
+                        <div className="input-group-text">
+                          /{item.product.unit}
+                        </div>
+                      </div>
+                    </td>
+                    <td className="text-end">
+                      {item.final_total !== item.total && (
+                        <small className="text-success">折扣價：</small>
+                      )}
+                      {currency(item.final_total)}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td colSpan="3" className="text-end">
+                    總計
+                  </td>
+                  <td className="text-end">{currency(cart?.total)}</td>
+                </tr>
+                {cart?.final_total !== cart?.total ? (
+                  <tr>
+                    <td colSpan="3" className="text-end text-success">
+                      折扣價
+                    </td>
+                    <td className="text-end text-success">
+                      {currency(cart?.final_total)}
+                    </td>
+                  </tr>
+                ) : null}
+              </tfoot>
+            </table>
+            <div className="text-end">
+              <button
+                className="btn btn-outline-danger"
+                type="button"
+                onClick={deleteCartAll}>
+                清空購物車
+              </button>
+            </div>
+          </>
+        ) : (
+          <p>您的購物車是空的。</p>
+        )}
+      </div>
+    </div>
   );
 }
 
